5de19df61a1d0e0d68a4a0e425fbbee7df1e8237
Caleb Lima <lima.cardoso.caleb@gmail.com>
2025-05-20 11:44:34 -0300

    add note

--- src/plugins/sendAnalytics.ts
+++ src/plugins/sendAnalytics.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest } from "fastify";
 import fp from "fastify-plugin";
 
+//usar fastify-plugin somente quando for um plugin que toda aplicação irá usar
 export const sendAnalytics = fp((fastify) => {
   fastify.decorate("sendAnalytics", (request: FastifyRequest) => {
     console.log(
